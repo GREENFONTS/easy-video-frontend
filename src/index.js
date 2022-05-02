5c5e52c43cbbfe54aa5aca8b91cd59dbfd7f852d
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-05-02 17:22:39 +0100

    updated index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
